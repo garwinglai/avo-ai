ca6644c3f918229ca4f722cb6c7ceef887a35b9f
Garwing <garwinglai@gmail.com>
2024-12-25 13:38:10 -0800

    Firebase Functions Init

---
+++ functions/index.js
@@ -0,0 +1,67 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const axios = require("axios");
+require("dotenv").config();
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.addmessage = onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await getFirestore()
+    .collection("messages")
+    .add({ original: original });
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+exports.getFatSecretToken = onRequest(async (req, res) => {
+  const clientId = process.env.FATSECRET_CLIENT_ID;
+  const clientSecret = process.env.FATSECRET_CLIENT_SECRET;
+  const accessTokenURL = "https://oauth.fatsecret.com/connect/token";
+
+  try {
+    const response = await axios.post(
+      accessTokenURL,
+      new URLSearchParams({
+        grant_type: "client_credentials",
+        scope: "basic",
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        auth: {
+          username: clientId,
+          password: clientSecret,
+        },
+      }
+    );
+
+    const newToken = response.data.access_token;
+    console.log("newToken", newToken);
+
+    res.status(200).send(newToken);
+  } catch (error) {
+    console.error("Error fetching FatSecret token:", error);
+    res.status(500).send("Error fetching FatSecret token");
+  } finally {
+    console.log("finally");
+  }
+});
