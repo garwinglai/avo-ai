0229b11b8cd3abb782d6f9a361cf270f14469ea6
Garwing <garwinglai@gmail.com>
2024-12-24 19:54:10 -0800

    Firestore Setup

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -1,12 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
 import { EvilIcons } from "@expo/vector-icons";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../../firebase/firebaseConfig";
 
 const HomeTab = () => {
+  const handleClick = async () => {
+    console.log("clicked");
+
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
   return (
     <View>
       <Text className=" text-2xl text-red-400">HomeTab</Text>
       <EvilIcons name="camera" size={24} color="green" />
+      <Pressable onPress={handleClick}>
+        <Text>Press</Text>
+      </Pressable>
     </View>
   );
 };
